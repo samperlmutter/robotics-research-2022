18d2724ba882aef5830fb756dcf465fe45589cb6
Sam Perlmutter <sam.perlmutter227@gmail.com>
2022-02-22 15:21:01 -0500

    Refactored to python3 and implemented depth image parsing

--- scripts/contact_detection.py
+++ scripts/contact_detection.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import colorsys
@@ -11,67 +11,72 @@ import sensor_msgs.point_cloud2 as pc2
 from tf.transformations import quaternion_from_euler
 
 pc_pub = rospy.Publisher('/contact/cloud', Marker, queue_size=100)
+pose_proximity_radius = 0.1
+cy = 239.5
+cx = 319.5
+fx = 570.3422
+fy = 319.5
 
+
+def compute_image_pos(pose):
+    depth = 1
+    return (((pose.x * fx) / depth) + cx,
+            ((pose.y * fy) / depth) + cy)
+# make bag file of all topics
+# get distance from pose to camera
+# convert to pixel coords
+# check if nearby pixels are a similar depth
+# color them if yes
 
 def near_pose(pose_pos, point_pos):
-  if ((point_pos.x - pose_pos.x) ** 2) + ((point_pos.y - pose_pos.y) ** 2) + ((point_pos.z - pose_pos.z) ** 2) <= 0.1 ** 2:
-    return ColorRGBA(1, 0, 0, 1)
-  else:
-    return ColorRGBA(0.35, 0.35, 0.35, 1)
+    if ((point_pos.x - pose_pos.x) ** 2) + ((point_pos.y - pose_pos.y) ** 2) + (
+            (point_pos.z - pose_pos.z) ** 2) <= pose_proximity_radius ** 2:
+        return ColorRGBA(1, 0, 0, 1)
+    else:
+        return ColorRGBA(0.35, 0.35, 0.35, 1)
 
 
 def contact_cb(pose):
-  start = rospy.Time.now().to_time()
-  points = point_cloud_cb(rospy.wait_for_message('/camera/depth/points', PointCloud2))
+    start = rospy.Time.now().to_time()
+    points = point_cloud_cb(rospy.wait_for_message('/camera/depth/points', PointCloud2))
 
-  m = Marker()
-  m.scale = Vector3(.01, .01, .01)
-  m.color = ColorRGBA(1, 1, 1, 1)
-  m.type = Marker.POINTS
+    m = Marker()
+    m.scale = Vector3(.01, .01, .01)
+    m.color = ColorRGBA(1, 1, 1, 1)
+    m.type = Marker.POINTS
 
-  q = quaternion_from_euler(math.radians(90), math.radians(180), 0)
-  m.pose = Pose()
-  # m.pose.orientation.x = q[0]
-  # m.pose.orientation.y = q[1]
-  # m.pose.orientation.z = q[2]
-  # m.pose.orientation.w = q[3]
+    m.pose = Pose()
 
-  m.header.stamp = rospy.Time.now()
-  m.header.frame_id = 'camera_rgb_optical_frame'
+    m.header.stamp = rospy.Time.now()
+    m.header.frame_id = 'camera_rgb_optical_frame'
 
-  def calc_color(i):
-    mult = 360. / float(len(points))
-    return colorsys.hsv_to_rgb((i * mult) / 350., 1, 1)
+    colors = []
+    for i in range(len(points)):
+        colors.append(near_pose(pose.pose.position, points[i]))
+    m.colors = colors
+    m.points = points
 
+    rospy.loginfo(rospy.Time.now().to_time() - start)
 
-  colors = [None for i in range(len(points))]
-  for i in range(len(points)):
-    colors[i] = near_pose(pose.pose.position, points[i])
-    # c = calc_color(i)
-    # colors[i] = ColorRGBA(c[0], c[1], c[2], 1)
-  m.colors = colors
-  m.points = points
-
-  rospy.loginfo(rospy.Time.now().to_time() - start)
-  
-  pc_pub.publish(m)
+    pc_pub.publish(m)
 
 
 def point_cloud_cb(point_cloud):
-  points = []
-  points_gen = pc2.read_points(point_cloud, skip_nans=True)
-  for p in points_gen:
-    p = Point(p[0], p[1], p[2])
-    points.append(p)
+    points = []
+    points_gen = pc2.read_points(point_cloud, skip_nans=True)
+    for p in points_gen:
+        p = Point(p[0], p[1], p[2])
+        points.append(p)
 
-  return points
-  
+    return points
+
 
 def main():
-  rospy.init_node('contact_detector')
-  rospy.Subscriber('/contact/pose', PoseStamped, contact_cb)
+    rospy.init_node('contact_detector')
+    rospy.Subscriber('/contact/pose', PoseStamped, contact_cb)
 
-  rospy.spin()
+    rospy.spin()
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- scripts/read_depth.py
+++ scripts/read_depth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from sensor_msgs.msg import Image
@@ -6,20 +6,37 @@ import numpy as np
 from cv_bridge import CvBridge
 import cv2
 
+cy = 239.5
+cx = 319.5
+fx = 570.3422
+fy = 319.5
+
+
+def compute_world_pos(x, y, depth):
+    return ((x - cx) * depth / fx,
+            (y - cy) * depth / fy,
+            depth,
+            1)
+
 
 def main():
-  rospy.init_node('contact_detector')
-  img = rospy.wait_for_message('/camera/depth/image_raw', Image)
-  bridge = CvBridge()
-  cv_img = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
-  rospy.loginfo(len(img.data))
-  data = []
-  for i in range(0, len(img.data[:5]), 2):
-      rospy.loginfo((str.encode(img.data[i]) << 8) | str.encode(img.data[i+1]))
-    #   data.append((bytearray(img.data[i]) << 8) | bytearray(img.data[i+1]))
-  
-  rospy.loginfo(data)
+    rospy.init_node('contact_detector')
+    img = rospy.wait_for_message('/camera/depth/image_raw', Image)
+    bridge = CvBridge()
+    cv_img = np.array(bridge.imgmsg_to_cv2(img, desired_encoding='32FC1') / 1000)
+    rospy.loginfo(cv_img.shape)
+
+    img_pts = np.empty(cv_img.shape, dtype=object)
+    for y in range(len(img_pts)):
+        for x in range(len(img_pts[0])):
+            img_pts[y][x] = compute_world_pos(x, y, cv_img[y][x])
+
+    rospy.loginfo(img_pts[100][200])
+
+    # cv_img_gray = np.array((cv_img / max(map(max, cv_img))) * 255, dtype=np.uint8)
+    # cv2.imshow('depth', cv_img_gray)
+    # cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-  main()
+    main()
